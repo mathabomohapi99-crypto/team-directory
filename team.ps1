5075cf9dc83a99399310e9e6e0f7caf97a266a63
mathabomohapi99-crypto <mathabomohapi99@gmail.com>
2026-09-02 02:43:32 +0200

    Wire team script to read and display team data

--- team.ps1
+++ team.ps1
@@ -1,4 +1,10 @@
 Write-Host "=== Team Directory ==="
+
 function Get-TeamMembers {
     Get-Content -Path "team.txt"
+}
+
+$members = Get-TeamMembers
+foreach ($member in $members) {
+    Write-Host $member
 }
